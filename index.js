02c5549c6687df00728347a5d50ddc69bddbc51f
zeidhassan <zaid.hartha@gmail.com>
2026-01-30 22:33:29 +0800

    Initial bot setup

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
